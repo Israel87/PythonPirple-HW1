c31e763fef13a517e3a0b11340c142e4b0ee5686
Israel Nnaji <nnajiisrael@yahoo.com>
2018-12-28 16:30:35 +0100

    variables
    - PirplePython Assignment #1

---
+++ main.py
@@ -0,0 +1,38 @@
+#This is a python project that declares variables with different datatypes and the values assigned to them,
+#for this project, i will be looking at the various attributes associated with my favorite song.
+
+#strings 
+
+nameofSong = "Glorious Ruin"
+albumArtist = "Hillsong Live"
+album = "Glorious Ruins (Live) [Deluxe Edition]"
+genre = "Christian & Gospel"
+kind = "MPEG Audio File"
+channels = "Joint Stereo"
+encodedWith = "unknown"
+bitRate = "320 kbps"
+format = "MPEG-1, Layer 3"
+size = "20.5 MB"
+sampleRate = "44.100 kHz"
+
+
+
+#integers 
+
+durationInSeconds = 530
+
+# printed all at once 
+print(nameofSong,albumArtist, album, genre, kind, channels, encodedWith, bitRate, format, size, sampleRate )
+
+# printed one after the other 
+print(nameofSong)
+print(albumArtist)
+print(album)
+print(genre)
+print(kind)
+print(channels)
+print(encodedWith)
+print(bitRate)
+print(format)
+print(size)
+print(sampleRate)
